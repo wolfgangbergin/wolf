2e797d48ba050fcfaeaeca50b65bf4b3ab7de921
wolfgangbergin <66983551+wolfgangbergin@users.noreply.github.com>
2022-10-07 08:53:10 -0400

    3rd

--- scripts/css1/test1.js
+++ scripts/css1/test1.js
@@ -1,27 +1,13 @@
 
 export function test1(){
     let wolf = `
-    <div class="container">
-    <div class="container">
-    <div class='lopez' src="lopez.jpg" width="380" >
-    <div class="topleft">Top Left</div>
-    <div class="wolf5">wolf5</div>
-   
-    <div class="bergin">bergin</div>
-    </div>
-  
-    
-  </div>
+ 
  
  
     
-    <div class="cssPositioning">
-    <div class="wolf1">wolf1</div>
-    <div class="wolf2">wolf2</div>
-    <div class="wolf3">wolf3</div> 
-    
-    <div class="wolf4">wolf4</div>
+    <div class="test1">
    
+    <p class="test1">test1<p>
     
      </div>
     `
--- scripts/nav.js
+++ scripts/nav.js
@@ -1,4 +1,4 @@
-import { cssPositioning } from './Positioning.js';
+import { wolf1 } from './wolf1/wolf1.js';
 import { test1 } from './css1/test1.js';
 
 let cssButton = document.getElementById('cssPositioning');
@@ -6,16 +6,16 @@ let content = document.getElementById('content');
 let wolfgang = document.getElementById('wolfgang');
 let test = document.getElementById('test1');
 
-cssButton.addEventListener('click', () => nav('css positioning'));
+cssButton.addEventListener('click', () => nav('wolf3'));
 test.addEventListener('click', () => nav('test1'));
+
 
 function nav(param1) {
   switch (true) {
   
-    case param1 === 'css positioning':
-     
-      log(globalThis.document.body.childNodes[1].childNodes[1].innerHTML = 'wolf')
-      wolfgang.innerHTML = cssPositioning();
+    case param1 === 'wolf1':
+   
+      wolfgang.innerHTML = wolf1();
       break;
     case param1 === 'test1':
       
@@ -25,4 +25,91 @@ function nav(param1) {
       log('default');
   }
 }
-nav('css positioning');
+nav('wolf1');
+// let wolf4 = document.getElementsByTagName('li')
+// log(wolf4)
+// for (let element of wolf4){
+//   element.style.backgroundColor = 'hotpink'
+// } 
+
+// let wolf5 = document.querySelectorAll('#kim2')
+// log(wolf5)
+// for (let element of wolf5){
+//   element.style.backgroundColor = 'hotpink'
+// } 
+// let wolf6 = document.querySelectorAll('ul')
+// let fla = wolf6[1]
+
+// let bobo =  fla.querySelectorAll('#kim2')
+// log(bobo)
+// bobo[0].style.backgroundColor = 'hotpink'
+// let jobob =  fla.querySelector('#kim1')
+// log(jobob)
+// jobob.style.backgroundColor = 'green'
+
+// let mich = document.querySelector('ul#mich li:nth-of-type(4)')
+// mich.style.backgroundColor = 'hotpink'
+function wolfTest(){
+  log('wolfTest')
+}
+let fla = document.querySelector('#mich')
+fla.className = 'kim313'
+
+
+let kim = document.querySelector('.kim313')
+
+kim.style.backgroundColor = 'yellow'
+
+let odd = fla.querySelector(':nth-of-type(4)')
+
+ odd.style.backgroundColor = 'blue'
+fla.style.backgroundColor = 'green'
+let wolf = fla.querySelectorAll('.wolf')
+
+
+for (let element of wolf){
+  element.style.backgroundColor = 'hotpink'
+} 
+
+let mich = kim.querySelector(':nth-of-type(1)')
+let kim3 = kim.querySelector(':nth-of-type(3)')
+mich.style.color = 'red'
+mich.style.backgroundColor = 'yellow'
+mich.textContent = 'kim313'
+mich.style.border = " red 3px dotted"
+mich.lang = 'wolf'
+
+kim3.type = 'square'
+kim3.accesskey = 'k'
+
+
+
+let stress = document.querySelector('#stress')
+// dir(stress.hidden = true)
+
+stress.style.backgroundColor = 'red'
+stress.addEventListener('click', wolfTest)
+
+// dir(stress.style.display = 'none')
+stress.style.position = 'absolute'
+ kim3.style.backgroundColor = 'burlywood'
+kim3.style.position = 'relative'
+kim3.style.top = '50px'
+kim3.className = 'apple'
+
+let p = document.querySelector(".para1")
+p.style.backgroundColor = 'blue'
+p.style.backgroundColor = 'blue'
+p.style.position = 'relative'
+p.style.top = '100px'
+
+let p_prime = p.cloneNode(true)
+
+ log(p_prime)
+ let jobob = document.querySelector(".jobob")
+ jobob.appendChild(p_prime)
+// for (let element of wolf5){
+//   element.style.backgroundColor = 'hotpink'
+// } 
+
+
