1ad6f65df8d7da43e419cde962991134fbd33f83
wolfgangbergin <66983551+wolfgangbergin@users.noreply.github.com>
2022-10-11 08:13:35 -0400

    3rd

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,3 +1,5 @@
+
+
 import { wolf1 } from './wolf1/wolf1.js';
 import { test1 } from './css1/test1.js';
 
@@ -9,29 +11,19 @@ let test = document.getElementById('test1');
 cssButton.addEventListener('click', () => nav('wolf1'));
 test.addEventListener('click', () => nav('test1'));
 
-
 function nav(param1) {
   switch (true) {
-  
     case param1 === 'wolf1':
-  
       wolfgang.appendChild(wolf1());
       break;
     case param1 === 'test1':
-      wolfgang.innerHTML = null
-      wolfgang.appendChild(test1())
+      wolfgang.innerHTML = null;
+      wolfgang.appendChild(test1());
       break;
     default:
       log('default');
   }
 }
 nav('wolf1');
-
-
-const ALL = document.querySelectorAll("ul p, ul button")
-
-for (let i = 0; i< ALL.length; ++i){
-  ALL[i].style.backgroundColor = 'red'
-}
 
 
