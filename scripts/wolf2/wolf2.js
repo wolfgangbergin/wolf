6bd97d4d938f25c8148203ac4cc2d8a79978d683
wolfgangbergin <66983551+wolfgangbergin@users.noreply.github.com>
2022-10-13 10:33:25 -0400

    3rd

--- scripts/wolf2/wolf2.js
+++ scripts/wolf2/wolf2.js
@@ -4,8 +4,41 @@ export function wolf2(){
     let wolf = `
  
  
- <div>wolfgang2</div>
+<h1>Temperament<h1>
+<hr>
+<p>Captive wolves are generally shy and avoid eye contact with humans other than their owner,
+ as well as not listening to any commands made by any other humans. They usually vacate rooms or 
+ hide when a new person enters the establishment. Even seemingly friendly wolves need to be treated
+ with caution, as captive wolves tend to view and treat people as other wolves, and will thus bite or 
+ dominate people in the same situation
+ in which they would other wolves.</p>
+ <h1>Trainability<h1>
+ <hr>
+ <p>Though wolves are trainable, they lack the same degree of tractability seen in dogs. They are generally 
+ not as responsive as dogs to coercive techniques involving fear, aversion to stimuli, and force. Generally,
+  far more work is required to obtain the same degree of reliability seen in most dogs. Even then, once a 
+  certain behavior has been repeated several times, wolves may get bored and ignore subsequent commands. 
+  Wolves are most responsive toward positive conditioning and rewards, though simple praise is not sufficient 
+  as in most dogs.  Unlike dogs, wolves tend to respond more to hand signals than voice. Although they are
+   more difficult to control than dogs, they can be easier to teach if the 
+   motivation exists.
+   
+ Timber wolves and coyotes used as draught animals in northern Ontario, 1923.
+ 
+  <p>
+  <img alt="" src="//upload.wikimedia.org/wikipedia/commons/thumb/8/84/The-Wolf-and-his-Master.jpg/220px-The-Wolf-and-his-Master.jpg" decoding="async" class="thumbimage" srcset="//upload.wikimedia.org/wikipedia/commons/8/84/The-Wolf-and-his-Master.jpg 1.5x" data-file-width="328" data-file-height="475" width="220" height="319">
 
+  <h1>Rearing</h1>
+       <hr>
+       <p>Captive wolf puppies are usually taken from their mother at the age of 
+       14 days, preferably no later than 21 days. Wolf pups require more socialisation than dog pups, 
+       and will typically stop responding to socialisation at the age of 19 days, as opposed to dogs which can
+        still be socialised at the age of 16 weeks. For the first four months of their lives, wolf pups need to 
+        be kept isolated from adult canines, except for a few brief visits per week, in order for them to properly 
+        imprint on humans. Pups will typically develop behavioural abnormalities if raised without another
+         member of their own kind. Because wolf milk contains more arginine than can be found in puppy
+          milk substitutes, an arginine supplement is needed when 
+       feeding pups below the weaning age. Failure to do so can result in the pups developing cataracts.</p> 
     
  
     `
@@ -13,3 +46,4 @@ export function wolf2(){
      
  
  }
+
