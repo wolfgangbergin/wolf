a498928331de95767c67b6b9b2738e61b466e31a
wolfgangbergin <66983551+wolfgangbergin@users.noreply.github.com>
2022-10-14 07:50:44 -0400

    3rd

--- scripts/wolf1/wolf1.js
+++ scripts/wolf1/wolf1.js
@@ -6,25 +6,25 @@ export function wolf1(){
  
     
 <h1>Wolves as pets and working animals</h1>
-<hr>
-  <p>Wolves are sometimes kept as exotic pets,
-   and in some rarer occasions, as working animals. 
-   Although closely related to domesticated dogs, wolves
-   do not show the same tractability as dogs in living alongside 
-   humans, and generally, a greater amount of effort is required in order 
-   to obtain the same amount of reliability. Wolves also need much more space than dogs,
-   about 25 to 40 square kilometres (10 to 15 sq mi) so they can exercise.</p>
-   <p>
+
   
  
     `
      return wolf
 
-    return nfl
+    
     
 
 }
 
 
 
-   
+{/* <hr>
+<p>Wolves are sometimes kept as exotic pets,
+ and in some rarer occasions, as working animals. 
+ Although closely related to domesticated dogs, wolves
+ do not show the same tractability as dogs in living alongside 
+ humans, and generally, a greater amount of effort is required in order 
+ to obtain the same amount of reliability. Wolves also need much more space than dogs,
+ about 25 to 40 square kilometres (10 to 15 sq mi) so they can exercise.</p>
+ <p> */}
