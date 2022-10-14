6ee629c86e9ff9c0dfbf34a986584d85cf8110ce
wolfgangbergin <66983551+wolfgangbergin@users.noreply.github.com>
2022-10-14 15:48:44 -0400

    3rd

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,5 +1,3 @@
-
-
 import { wolf1 } from './wolf1/wolf1.js';
 import { wolf2 } from './wolf2/wolf2.js';
 
@@ -14,12 +12,10 @@ wolf_2.addEventListener('click', () => nav('wolf_2'));
 function nav(param1) {
   switch (true) {
     case param1 === 'wolf_1':
-     
       wolfgang.innerHTML = null;
       wolfgang.innerHTML = wolf1();
       break;
     case param1 === 'wolf_2':
-  
       wolfgang.innerHTML = null;
       wolfgang.innerHTML = wolf2();
       break;
@@ -29,4 +25,25 @@ function nav(param1) {
 }
 nav('wolf_1');
 
+const WOLFMAN1 = document.querySelector('#kim313');
+WOLFMAN1.style.backgroundColor = 'red';
+const WOLFMAN2 = document.querySelectorAll('.wolfMan');
 
+for (const El of WOLFMAN2) {
+  El.style.color = 'white';
+}
+
+const WOLFMAN3 = document.querySelector('#wolfgang p:first-of-type ');
+
+WOLFMAN3.style.margin = ' auto  ';
+const WOLFMAN4 = document.querySelector('hr.wolfMan ~ p');
+WOLFMAN4.style.maxWidth = '300px';
+const WOLFMAN5 = document.querySelectorAll('hr.wolfMan + p');
+for (const El of WOLFMAN5) {
+  El.innerHTML = 'wolfgang bergin';
+}
+const WOLFMAN6 = document.querySelector('.wolfMan:nth-child(3)');
+
+WOLFMAN6.style.fontSize = '4rem';
+const WOLFMAN7 = document.querySelector('.wolfMan:nth-child(3):hover');
+WOLFMAN7.style.color = 'blue';
